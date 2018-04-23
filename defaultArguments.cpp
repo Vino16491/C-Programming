42f94ea11b6d4ee8f063d288cdc94522629e74d2
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-04-23 20:42:20 +0530

    default arguments

---
+++ defaultArguments.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int main(){
+
+    float amount;
+
+    float value(float p, int n, float r = 0.15);
+    void printline(char ch = '*', int len = 40);
+
+    printline(); //uses default value for arguments, specified in prototyping
+
+    amount = value(5000.00, 5);
+
+    cout<<"\n Final Value = " << amount << "\n\n";
+    printline('='); //uses default value for 2 nd argument, specified in prototyping
+    return 0;
+}
+
+    float value(float p, int n, float r){
+
+        int year = 1;
+        float sum = p;
+
+        while (year <= n){
+
+            sum = sum * (1+r);
+            year = year +1;
+        }
+
+        return sum;
+    }
+
+void printline(char ch, int len){
+    for (int i = 1; i<=len; i++){
+       printf("%c", ch);
+    }
+    printf("\n");
+}
+
+
