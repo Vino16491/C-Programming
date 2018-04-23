3126cae31383ab35cc76c2b51f7354a81f985475
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-04-23 21:30:06 +0530

    Nesting member function

---
+++ nestedMemberFunction.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+class set{
+
+    int m,n;
+
+public:
+    void input(void);
+    void display(void);
+    int largest(void);
+};
+
+int set :: largest(){
+
+    if(m >= n){
+
+        return m;
+    }else {
+        return n;
+    }
+}
+
+void set :: input(void){
+
+    cout<< "Input values of m and n" << "\n";
+    cin>> m >> n;
+
+}
+
+void set ::display() {
+
+    cout<< "Largest value = " << largest() << "\n";
+}
+
+int main() {
+
+    set A;
+
+    A.input();
+    A.display();
+
+    return 0;
+}
